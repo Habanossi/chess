6020594b61d90818b717523791d1af354e07eb09
Welcome to the hood <hermanni.viljanen@gmail.com>
2023-03-13 15:58:20 +0200

    restructuring

--- run.cpp
+++ run.cpp
@@ -18,27 +18,24 @@ void run(){
 	//start game
 	cout << "Wanna play chess? cool beans!\n";
 
-	int pos; //current position, position the piece is moving from
+	int pos = 1; //current position, position the piece is moving from
 	int n_pos = 1; //new position, position the piece is moving to
 
-	while(n_pos != 0){
+	while(pos != 0){
 		board_print(board, char_list);
 		bool c = 1;
 		while(c){
-			cout << "From where to move? ";
-			cin >> pos;
-			if(pos == 0){
+			pos = get_pos_command();
+			if(pos == 0){ // check for quitting command 0
 				c = 0;
-				n_pos = 0;
 				break;
 			}
 			for(auto& i : pieces){
 				if(i.getPos() != pos) continue;
 				bool l = 1;
 				while(l){
-					cout << "Move to: ";
-					cin >> n_pos;
-					if(board[n_pos/10 - 1][n_pos%10 - 1] == 0) l = 0;
+					n_pos = get_n_pos_command();
+					if(is_cell_empty(board, n_pos)) l = 0;
 					else cout << "can't move there.\n";
 				}
 				int k = i.getPos();
